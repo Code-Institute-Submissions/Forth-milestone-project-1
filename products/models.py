16eb53c0ce3949eb7c24e60c467ac7cbc967013f
plxsas <sale12302006@yahoo.com>
2020-11-18 21:09:15 +0000

    adding data

--- products/models.py
+++ products/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=254)
-    brand = models.CharField(max_length=254)
-    category = models.CharField(max_length=254)
-    sub_categories = models.CharField(max_length=254)
+    name = models.CharField(max_length=254, null=True, blank=True)
+    brand = models.CharField(max_length=254, null=True, blank=True)
+    category = models.CharField(max_length=254, null=True, blank=True)
+    sub_categories = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
-    url1 = models.URLField(max_length=1024)
-    color1 = category = models.CharField(max_length=254)
-    url2 = models.URLField(max_length=1024)
-    color2 = category = models.CharField(max_length=254)
-    url3 = models.URLField(max_length=1024)
-    color3 = category = models.CharField(max_length=254)
-    url4 = models.URLField(max_length=1024)
-    color4 = category = models.CharField(max_length=254)
+    url1 = models.URLField(max_length=1024, null=True, blank=True)
+    color1 = models.CharField(max_length=254, null=True, blank=True)
+    url2 = models.URLField(max_length=1024, null=True, blank=True)
+    color2 = models.CharField(max_length=254, null=True, blank=True)
+    url3 = models.URLField(max_length=1024, null=True, blank=True)
+    color3 = models.CharField(max_length=254, null=True, blank=True)
+    url4 = models.URLField(max_length=1024, null=True, blank=True)
+    color4 = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.product_name
+        return self.name
+        
