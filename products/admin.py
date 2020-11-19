1b7b02fb6091268d8c3e10206f68ef6e0df91187
plxsas <sale12302006@yahoo.com>
2020-11-19 15:44:36 +0000

    adding the database

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import database
 
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'brand',
+        'category',
+        'sub_categories',
+        'price',
+    )
+
+    ordering = ('brand',)
+admin.site.register(database, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=254, null=True, blank=True)
-    brand = models.CharField(max_length=254, null=True, blank=True)
-    category = models.CharField(max_length=254, null=True, blank=True)
-    sub_categories = models.CharField(max_length=254, null=True, blank=True)
+class database(models.Model):
+    name = models.CharField(max_length=254, null=True, blank=True, default='')
+    brand = models.CharField(max_length=254, null=True, blank=True, default='')
+    category = models.CharField(max_length=254, null=True, blank=True, default='')
+    sub_categories = models.CharField(max_length=254, null=True, blank=True, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
     url1 = models.URLField(max_length=1024, null=True, blank=True)
-    color1 = models.CharField(max_length=254, null=True, blank=True)
+    color1 = models.CharField(max_length=254, null=True, blank=True, default='')
     url2 = models.URLField(max_length=1024, null=True, blank=True)
-    color2 = models.CharField(max_length=254, null=True, blank=True)
+    color2 = models.CharField(max_length=254, null=True, blank=True, default='')
     url3 = models.URLField(max_length=1024, null=True, blank=True)
-    color3 = models.CharField(max_length=254, null=True, blank=True)
+    color3 = models.CharField(max_length=254, null=True, blank=True, default='')
     url4 = models.URLField(max_length=1024, null=True, blank=True)
-    color4 = models.CharField(max_length=254, null=True, blank=True)
+    color4 = models.CharField(max_length=254, null=True, blank=True, default='')
 
     def __str__(self):
-        return self.name
-        
+        return self.name     
