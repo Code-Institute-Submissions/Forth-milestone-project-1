e64f673850c3549a2c5d65f23b32d278cce0fd8a
plxsas <sale12302006@yahoo.com>
2020-11-21 17:15:28 +0000

    setting up product page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from products.models import database
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/index.html')
+    products = database.objects.all().order_by('?')[:12]
+
+    context = {
+        'products': products
+    }
+    return render(request, 'home/index.html', context)
