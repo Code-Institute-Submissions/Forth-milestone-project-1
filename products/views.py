9925c498e343aa496f857c480cd1039d332ce55d
plxsas <sale12302006@yahoo.com>
2020-11-21 12:38:18 +0000

    making product page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Count
+from django.db.models.aggregates import Max
+from .models import database
 
 # Create your views here.
+
+def all_products(request):  
+    products = database.objects.all()
+    counts = database.objects.values('sub_categories').annotate(count_subcategories=Count('name')).order_by()
+    images = database.objects.values('sub_categories').annotate(latest_date=Max('url1')).order_by()
+    context = {
+        'products': products,
+        'counts': counts,
+        'images': images
+    }
+    return render(request, 'products/products.html', context)
