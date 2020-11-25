2cec554b1dc54c84c164b3b8a6a985cfbca6ce26
plxsas <sale12302006@yahoo.com>
2020-11-25 12:24:26 +0000

    soleved part of the problem

--- products/views.py
+++ products/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Count
 from django.db.models.aggregates import Max
 from .models import database
 
 # Create your views here.
 
-def all_products(request):  
+def all_products(request):
     products = database.objects.all()
-    counts = database.objects.values('sub_categories').annotate(count_subcategories=Count('name'),latest_url=Max("url1")).order_by()
+    
 
+    if request.GET: 
+        brand = request.GET.get("brand")
+        category = request.GET.get("category")
+        products = database.objects.filter(category=category)
+        products = database.objects.filter(brand=brand)
+        products = database.objects.filter(sub_categories=brand)
+        
+       
+        
+    
+    
     context = {
-        'products': products,
-        'counts': counts
-    }
-    return render(request, 'products/products.html', context)
+        'products': products
+        }
+    return render(request, 'products/products2.html', context)
+
