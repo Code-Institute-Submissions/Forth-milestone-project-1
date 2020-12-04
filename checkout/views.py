08ce133d612ae5e96354f383e30dcb4730448020
plxsas <sale12302006@yahoo.com>
2020-12-04 12:10:27 +0000

    added checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
 
-# Create your views here.
+from django.contrib import messages
+from django.conf import settings
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import database
+from bag.contexts import bag_contents
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        return redirect(reverse('all_products'))
+
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
